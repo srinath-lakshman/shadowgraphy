d19e08a7bff54c7720dd3a4943fbb188b20b799c
srinath-lakshman <s.lakshman@utwente.nl>
2020-01-29 14:12:48 +0100

    Completed the pixel length scale

--- 1a. speed1_pixel_length.py
+++ 1a. speed1_pixel_length.py
@@ -4,50 +4,113 @@ import glob
 from skimage import io
 import numpy as np
 from PIL import Image
+from skimage.filters import sobel
+from skimage.filters import threshold_otsu
+from skimage import filters
 
 ################################################################################
 
-hard_disk   = r'/media/devici/Samsung_T5/'
-project     = r'srinath_dhm/impact_over_thin_films/speed1/00100cs0010mum_r4/'
+hard_disk   = r'/media/devici/Samsung_T5'
+project     = r'srinath_dhm/impact_over_thin_films/speed1/00100cs0010mum_r4'
 
-f = hard_disk + project + r'00100cs0010mum_r4'
+################################################################################
+
+f = hard_disk + '/' + project + '/' + r'00100cs0010mum_r4'
 os.chdir(f)
 
-image = io.imread(r"screw_diameter_mm.tif")
+image_name = r"screw_diameter_mm"
 
-binary = image > 1000
+image = io.imread(image_name + ".tif")
+bit_depth = int(tuple(open(image_name + ".cih",'r'))[26][12:])
 
-x_px = np.shape(image)[0]
-y_px = np.shape(image)[1]
+image_conv = image * (((2**16)-1)/((2**bit_depth)-1))
 
-y_start = 360
-y_end = 700
+gray_filter = filters.gaussian(image_conv)
+edge_sobel = sobel(gray_filter)
+threshold = int(threshold_otsu(edge_sobel))
+binary = edge_sobel > threshold
 
-diff = np.zeros(y_end - y_start + 1, dtype='float')
+fig_rows = 2
+fig_columns = 2
 
-for j in np.arange(y_start, y_end+1, 1):
-    diff[int(j-y_start)] = (1 - np.mean(binary[j,:]))*x_px
+fig1 = plt.figure(1, figsize=(8,8))
+ax1 = plt.subplot(fig_rows,fig_columns,1)
+ax1.imshow(image_conv, cmap='gray')
+ax1.set_xticks([])
+ax1.set_yticks([])
+ax1.set_title("Grayscale Image, 16 bit")
 
-f = plt.figure(1)
-ax1 = plt.subplot(1,3,1)
-ax1.imshow(image, cmap=plt.get_cmap('gray'))
+ax2 = plt.subplot(fig_rows,fig_columns,2)
+ax2.imshow(gray_filter, cmap='gray')
+ax2.set_xticks([])
+ax2.set_yticks([])
+ax2.set_title("Gaussian fliter")
 
-ax2 = plt.subplot(1,3,2)
-ax2.imshow(binary, cmap=plt.get_cmap('gray'))
+ax3 = plt.subplot(fig_rows,fig_columns,3)
+ax3.imshow(edge_sobel, cmap='gray')
+ax3.set_xticks([])
+ax3.set_yticks([])
+ax3.set_title("Edge Sobel")
 
-ax3 = plt.subplot(1,3,3)
-ax3.plot(diff)
-ax3.axis('equal')
+ax4 = plt.subplot(fig_rows,fig_columns,4)
+ax4.imshow(binary, cmap='gray')
+ax4.set_xticks([])
+ax4.set_yticks([])
+ax4.set_title(f"Binary Image, Threshold = {threshold}")
 
-# f.set_aspect('equal')
 plt.show(block=False)
 
-avg_diff = int(np.mean(diff))
+x_px, y_px = list(np.shape(image))
 
+y_extents = np.array(input('Vertical y-extents = ').split(',')).astype('int')
+
+ax1.axhline(y=y_extents[0], linestyle='--', color='red')
+ax1.axhline(y=y_extents[1], linestyle='--', color='red')
+
+ax2.axhline(y=y_extents[0], linestyle='--', color='red')
+ax2.axhline(y=y_extents[1], linestyle='--', color='red')
+
+ax3.axhline(y=y_extents[0], linestyle='--', color='red')
+ax3.axhline(y=y_extents[1], linestyle='--', color='red')
+
+ax4.axhline(y=y_extents[0], linestyle='--', color='red')
+ax4.axhline(y=y_extents[1], linestyle='--', color='red')
+
+y_start = min(y_extents[0], y_extents[1])
+y_end = max(y_extents[0], y_extents[1])
+
+y_line = np.arange(y_start, y_end+1, 1)
+n = len(y_line)
+# diff = np.zeros(n)
+coor_left = np.zeros((n,2))
+coor_right = np.zeros((n,2))
+
+for i in range(n):
+    coor_left[i] = np.mean(np.where(binary[y_line[i],0:int(x_px/2)]==1)), y_line[i]
+    coor_right[i] = int(x_px/2) + np.mean(np.where(binary[y_line[i],int(x_px/2):x_px]==1)), y_line[i]
+
+x_left_avg = np.mean(coor_left[:,0])
+x_right_avg = np.mean(coor_right[:,0])
+
+ax1.plot([x_left_avg, x_left_avg], [y_start, y_end], linestyle='--', color='red')
+ax1.plot([x_right_avg, x_right_avg], [y_start, y_end], linestyle='--', color='red')
+
+ax2.plot([x_left_avg, x_left_avg], [y_start, y_end], linestyle='--', color='red')
+ax2.plot([x_right_avg, x_right_avg], [y_start, y_end], linestyle='--', color='red')
+
+ax3.plot([x_left_avg, x_left_avg], [y_start, y_end], linestyle='--', color='red')
+ax3.plot([x_right_avg, x_right_avg], [y_start, y_end], linestyle='--', color='red')
+
+ax4.plot([x_left_avg, x_left_avg], [y_start, y_end], linestyle='--', color='red')
+ax4.plot([x_right_avg, x_right_avg], [y_start, y_end], linestyle='--', color='red')
+
+plt.show(block=False)
+
+length_px = abs(x_right_avg-x_left_avg)
 length_mm = np.loadtxt("screw_diameter_mm.txt")
-px = round((length_mm/avg_diff)*(10.0**3),3)
+px_microns = round((length_mm/length_px)*(10.0**3),3)
 
-input(px)
-
+input(f"1 pixel = {px_microns} microns")
+np.savetxt("px_microns.txt", [px_microns], fmt='%0.3f')
 
 ################################################################################
