b5b162146c6a2f9fb13e60b6865855cd824bca92
Srinath Lakshman <45358318+srinath-lakshman@users.noreply.github.com>
2020-12-09 17:44:30 +0100

    Create 3a. drop_contour.py

---
+++ 3a. drop_contour.py
@@ -0,0 +1,124 @@
+import os
+import matplotlib.pyplot as plt
+import glob
+from skimage import io
+import numpy as np
+from PIL import Image
+from skimage.filters import sobel
+from skimage.filters import threshold_otsu
+from skimage import filters
+from skimage import measure
+
+################################################################################
+
+directory = r'E:/color_interferometry/side_view/20200520/experiment/'
+sub_directory = r'needle_tip_45_mm_from_surface/impact_on_glass_r1/'
+
+################################################################################
+
+os.chdir(directory)
+px_microns = np.loadtxt(r'px_microns.txt')
+
+os.chdir(sub_directory)
+images = io.ImageCollection(sorted(glob.glob('*.tif'), key=os.path.getmtime))
+n = len(glob.glob('*.tif')) - 1
+
+start_image     = int('028')
+contact_image   = int('114')
+end_image       = int('228')
+
+# start_image = int('101')
+
+# image_cropped = image[y_min:y_max+1,x_min:x_max+1]
+# image_cropped_filter = filters.gaussian(image_cropped)
+# edge_sobel = sobel(image_cropped_filter)
+# threshold = threshold_otsu(edge_sobel)
+# binary = edge_sobel > threshold
+
+y_wall = 795
+x_center = 500
+
+# plt.imshow(images[start_image-1], cmap='gray')
+# plt.show()
+
+ylimits = [y_wall-400, y_wall]
+xlimits = [x_center-200, x_center+200]
+
+fig, ax = plt.subplots(1,1)
+
+for i in range(start_image, end_image+1):
+    image_cropped = images[i][ylimits[0]:ylimits[1], xlimits[0]:xlimits[1]]
+    image_cropped_filter = filters.gaussian(image_cropped)
+    edge_sobel = sobel(image_cropped_filter)
+    threshold = threshold_otsu(edge_sobel)
+    binary = edge_sobel > threshold
+
+    binary[50:80, 20:50] = 0
+    binary[30:60, 330:360] = 0
+
+    # plt.imshow(binary, cmap='gray')
+    # plt.show()
+
+    binary_x = np.argwhere(binary == True)[:,1]
+    binary_y = np.argwhere(binary == True)[:,0]
+
+    coor_left = []
+    for y_avg in np.arange(np.min(binary_y), np.max(binary_y)+1,1):
+        indices = np.argwhere(binary_y == y_avg)
+        x_avg = binary_x[indices]
+        x_left_values = x_avg[np.argwhere(binary_x[indices] <= 200)[:,0]]
+        if len(x_left_values) != 0:
+            x_left_min = np.min(x_left_values)
+            coor_left.append([x_left_min, y_avg])
+
+    coor_right = []
+    for y_avg in np.arange(np.max(binary_y), np.min(binary_y)-1,-1):
+        indices = np.argwhere(binary_y == y_avg)
+        x_avg = binary_x[indices]
+        x_right_values = x_avg[np.argwhere(binary_x[indices] >= 200)[:,0]]
+        if len(x_right_values) != 0:
+            x_right_max = np.max(x_right_values)
+            coor_right.append([x_right_max, y_avg])
+    coor_left = np.array(coor_left)
+    coor_right = np.array(coor_right)
+    coor = np.vstack((np.flipud(coor_left),np.flipud(coor_right)))
+    p10 = np.poly1d(np.polyfit(coor[:,1], coor[:,0], 10))
+    p10_left = np.poly1d(np.polyfit(coor_left[:,1], coor_left[:,0], 10))
+    p10_right = np.poly1d(np.polyfit(coor_right[:,1], coor_right[:,0], 10))
+
+    # ax.imshow(image_cropped,cmap='gray')
+    # ax.plot(p10_right(coor_right[:,1]),coor_right[:,1],marker='.',color='blue')
+
+    # ax.set_xticks([])
+    # ax.set_yticks([])
+    # ax.set_xticklabels([])
+    # ax.set_yticklabels([])
+    # sampling_y = np.linspace(np.min(coor_right[:,1]), np.max(coor_right[:,1]), 100)
+    # sampling_x = p10_right(sampling_y)
+    # sampling_x = np.flipud(sampling_x)
+    # sampling_y = np.flipud(sampling_y)
+    # x_microns = (sampling_x - 107)*px_microns
+    # y_microns = (99 - sampling_y)*px_microns
+    x_microns = (p10_right(coor_right[:,1]) - 107)*px_microns
+    y_microns = (99 - coor_right[:,1])*px_microns
+    x_mm = x_microns/1000
+    y_mm = y_microns/1000
+    ax.scatter(x_mm, y_mm, marker='.',color='black')
+    # ax.axis('equal')
+    ax.set_aspect('equal', 'box')
+    ax.set_xlim(-0.25,2)
+    ax.set_ylim(-0.25,2.25)
+    ax.set_xticks([0, 0.50, 1, 1.50, 2])
+    ax.set_yticks([0, 0.50, 1, 1.50, 2])
+    ax.set_xlabel(r'r [$mm$]')
+    ax.set_ylabel(r'h [$mm$]')
+    ax.axhline(y=0,linestyle='--',color='black')
+    ax.axvline(x=0,linestyle='--',color='black')
+    plt.show()
+    # plt.tight_layout()
+
+    # plt.savefig('drop_outer_profile_' + f'{i:06d}' + '.png')
+    # np.savetxt('outer_profile_' + f'{i:06d}' + '.txt', np.column_stack([x_mm, y_mm]), fmt='%1.3f')
+    # plt.cla()
+
+################################################################################
