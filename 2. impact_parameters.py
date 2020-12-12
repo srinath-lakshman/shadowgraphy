bdd1f3c8af46483fc042b2a53035b3482bf083cb
srinath-lakshman <s.lakshman@utwente.nl>
2020-12-12 04:00:02 +0100

    Perfect code!

--- 2. impact_parameters.py
+++ 2. impact_parameters.py
@@ -42,7 +42,8 @@ fps_hz = int(tuple(open("oil_1cSt_impact_H_4R_on_oil_350cSt_15mum_.cih",'r'))[15
 
 images = io.ImageCollection(sorted(glob.glob('*.tif'), key=os.path.getmtime))
 n = len(glob.glob('*.tif')) - 1
-k_start, k_end = 21, 66
+
+k_start, k_end = 16, 66
 
 # plt.subplot(1,2,1)
 # plt.imshow(images[k_start],cmap='gray')
@@ -50,120 +51,83 @@ k_start, k_end = 21, 66
 # plt.imshow(images[k_end],cmap='gray')
 # plt.show()
 
-y_min, y_max = 675, 999
-x_min, x_max = 130, 380
-radii_min, radii_max = 95, 105
+# y_min, y_max = 675, 999
+
+y_wall = 1000
+y_top = 680
+x_center = int(np.shape(images)[2]/2)
+y_min, y_max = y_wall-360, y_wall+100
+x_min, x_max = x_center-150, x_center+150
 
 centers = np.zeros((k_end-k_start+1,2), dtype=int)
 diameters = np.zeros(k_end-k_start+1, dtype=int)
+volume = np.zeros((k_end-k_start+1), dtype=int)
 time_millisec = np.arange(0,((k_end-k_start+1)*1000.0)/fps_hz,1000.0/fps_hz, dtype=float)
+# time_millisec = time_millisec - time_millisec[-1]
+
+threshold = 670
+radius = 35
 
 for k in range(k_start, k_end+1):
-    j=0
 
-    x=np.zeros(k_end-k_start+1)
-    y=np.zeros(k_end-k_start+1)
-
+    print(k)
     image = images[k]
     image_cropped = image[y_min:y_max+1,x_min:x_max+1]
+    image_cropped_filter = filters.median(image_cropped)
+    binary = image_cropped_filter < threshold
+    binary[y_wall-y_min:,:] = 0
+    binary[:y_top-y_min,:] = 0
+    closing = morphology.closing(binary, morphology.disk(radius))
+    boundary = segmentation.find_boundaries(closing, connectivity=1, mode='outer', background=0)
+    indices = np.transpose(np.where(boundary == 1))
+    indices[:,[0,1]] = indices[:,[1,0]]
+    indices = indices[indices[:, 1].argsort()]
+    n = np.shape(indices)[0]
 
-    # image_modified = util.img_as_ubyte(image_cropped)
-    image_modified = image_cropped
+    axis = np.mean(indices[:,0])
+    vol = (np.pi/2)*np.trapz(np.power(indices[:,0]-axis,2))
+    xx = axis
+    yy = (np.pi/2)*np.trapz(indices[:,1]*np.power(indices[:,0]-axis,2))/vol
 
-    #blurring image using median filter
-    image_cropped_filter = filters.median(image_modified)
-
-    #edge detection
-    edge_sobel = filters.sobel(image_cropped_filter)
-    # edge_sobel = feature.canny(image_cropped_filter)
-
-    #closing
-    # closing = morphology.closing(edge_sobel, morphology.disk(2))
-    # morphology.disk(20)
-
-    #thresholding
-    threshold = threshold_otsu(edge_sobel)
-    binary = edge_sobel > threshold
-
-    boundary = segmentation.find_boundaries(binary, connectivity=1, mode='inner', background=0)
-
-    #edge skeleton
-    drop_skeleton = morphology.skeletonize(binary)
-
-    label_image = measure.label(drop_skeleton==1, connectivity=2)
-    image_label_overlay = color.label2rgb(label_image, image=drop_skeleton)
-
-    print(np.shape(label_image))
-
-    props = measure.regionprops(label_image, intensity_image=drop_skeleton)
-
-    # [x,y] = np.array(props[0:8].centroid
-
-    perimeter = np.zeros(label_image.max())
-
-    for i in range(label_image.max()):
-        perimeter[i] = props[i].perimeter
-
-    index = np.argmax(perimeter)
-
-    [x[j], y[j]] = props[index].centroid
-    xy_coor = props[index].coords
-    # print(props[index])
-    # input('')
-
-    # contour = measure.find_contour(labels == props[index].label, 0.5)[0]
-
-    # M = moments(image)
-    # centroid = (M[1, 0] / M[0, 0], M[0, 1] / M[0, 0])
+    centers[k-k_start] = [axis, yy]
+    volume[k-k_start] = vol
 
     plt.subplot(2,3,1)
     plt.imshow(image_cropped,cmap='gray')
-    plt.scatter(xy_coor[:,1], xy_coor[:,0])
     plt.title('Raw image')
+    plt.axis('off')
     plt.subplot(2,3,2)
-    # plt.hist(image_cropped)
     plt.imshow(image_cropped_filter,cmap='gray')
-    # plt.title('Median Filtered image')
+    plt.title('Median Filtered image')
+    plt.axis('off')
     plt.subplot(2,3,3)
-    plt.imshow(edge_sobel,cmap='gray')
-    # plt.title('Sobel Edge image')
-    plt.subplot(2,3,4)
     plt.imshow(binary,cmap='gray')
-    # plt.title('Watershed image')
+    plt.title('Binary otsu image')
+    plt.axis('off')
+    plt.subplot(2,3,4)
+    plt.imshow(closing,cmap='gray')
+    plt.title('Closing image')
+    plt.axis('off')
     plt.subplot(2,3,5)
-    plt.imshow(drop_skeleton,cmap='gray')
-    # plt.plot(peak_idx[:,1],peak_idx[:,0],'r.')
-    # # plt.title('Segmentation image')
+    plt.imshow(boundary,cmap='gray')
+    plt.title('Boundary image')
+    plt.axis('off')
     plt.subplot(2,3,6)
-    plt.imshow(label_image)
-    # plt.title('Segmentation Color image')
-    # plt.subplot(3,3,7)
-    # plt.imshow(color.label2rgb(labels, image=image_modified, kind='avg'), cmap='gray')
-    # # plt.title('Segmentation Geayscale image')
-    plt.tight_layout()
-    plt.show()
-
-    j = j+1
-
-    plt.imshow(edge_sobel, cmap='gray')
-    plt.xticks([])
-    plt.yticks([])
+    plt.imshow(image_cropped,cmap='gray')
+    plt.scatter(indices[:,0], indices[:,1])
+    plt.scatter(xx, yy)
+    plt.title('Center image')
+    plt.axis('off')
     plt.savefig('image_cropped' + str(k) +'.png', bbox_inches='tight', format='png')
     plt.close()
+    # plt.show()
 
-# diameter_mm = diameters[:]*(px_microns/1000)
-diameter_mm = 2
+xc_mm = abs(centers[:,0]-centers[0,0])*(px_microns/1000.0)
+yc_mm = abs(centers[:,1]-(y_wall-y_min+1))*(px_microns/1000.0)
 
-plt.subplot(1,2,1)
-plt.scatter(range(len(x)),x)
-plt.subplot(1,2,2)
-plt.scatter(range(len(y)),y)
-plt.show()
-
-xc_mm = abs(centers[:,1]-centers[0,1])*(px_microns/1000.0)
-yc_mm = abs(centers[:,0]-(y_max-y_min+1))*(px_microns/1000.0)
-
-avg_diameter_mm = np.mean(diameters[:])*(px_microns/1000)
+diameters = np.power((6*volume)/np.pi,1/3)
+diameter_mm = diameters*(px_microns/1000)
+avg_diameter_mm = np.mean(diameter_mm)
 diameter_mm_fitted = np.ones(len(time_millisec))* avg_diameter_mm
 
 linear_params = np.polyfit(time_millisec, xc_mm, 1)
@@ -183,12 +147,12 @@ fall_time_ms = abs(time_millisec[0] - time_millisec[-1])
 plt.figure(figsize=(15, 10))
 
 plt.subplot(2,1,1)
-plt.scatter(time_millisec,diameter_mm, marker='.', color='black')
-plt.plot(time_millisec,diameter_mm_fitted, linestyle='--', color='black')
+plt.scatter(time_millisec,diameter_mm/2, marker='.', color='black')
+plt.plot(time_millisec,diameter_mm_fitted/2, linestyle='--', color='black')
 plt.xlabel('$t$ $[ms]$')
-plt.ylabel('$D_{w}(t)$ $[mm]$')
-plt.ylim(0,2.0*avg_diameter_mm)
-plt.title(r'$D_{w}$ $\approx$ ' + str(round(avg_diameter_mm,3)) + r' $mm$, $v_{w}$ $\approx$ ' + str(abs(round(vy_impact_ms,3))) + r' $m/s$, $\Delta H_{w}$ $\approx$ ' + str(abs(round(fall_height_mm,3))) + r' $mm$, $\Delta t_{w}$ $\approx$ ' + str(abs(round(fall_time_ms,3))) + ' $ms$')
+plt.ylabel('$R_{w}(t)$ $[mm]$')
+plt.ylim(0.5,1.5)
+plt.title(r'$R_{w}$ $\approx$ ' + str(round(avg_diameter_mm/2,3)) + r' $mm$, $v_{w}$ $\approx$ ' + str(abs(round(vy_impact_ms,3))) + r' $m/s$, $\Delta H_{w}$ $\approx$ ' + str(abs(round(fall_height_mm,3))) + r' $mm$, $\Delta t_{w}$ $\approx$ ' + str(abs(round(fall_time_ms,3))) + ' $ms$')
 
 plt.subplot(2,2,3)
 plt.scatter(time_millisec,xc_mm, marker='.', color='black')
@@ -212,7 +176,7 @@ txt_file.write(f"Recording speed = {fps_hz} Hz\n")
 txt_file.write(f"Image number = {k_start, k_end}\n")
 txt_file.write(f"Y-limits = {y_min, y_max} pixels\n")
 txt_file.write(f"X-limits = {x_min, x_max} pixels\n")
-txt_file.write(f"Radii limits = {radii_min, radii_max} pixels\n")
+# txt_file.write(f"Radii limits = {radii_min, radii_max} pixels\n")
 txt_file.close()
 
 ################################################################################
